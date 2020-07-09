10c73fd85da6039d2e58b4fed7fe9cc6ce6a3298
Mirade McGloin <mirade.mcgloin@gmail.com>
2020-07-09 14:11:58 -0600

    adding script file and first click event

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//button 1 = city-search
+//button 2 = more-info
+//button 3 = compare
+//button 4 = compare2
+//button 5 = save-item
+//button 6 = search-again
+
+$('#city-search').on('click', function(){
+//function search cities
+
+
+})
+function searchCities(){
+    //sift through object
+}
