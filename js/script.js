de60763219579a40410b0c03d1e00e88a2902667
macT <simmonst208@gmail.com>
2020-07-12 20:13:41 -0600

    commit with corey as supervisor

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// GLOBAL VARIABLES
+let current = {};
+let saved = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+
 //button 1 = city-search
 //button 2 = more-info
 //button 3 = compare
@@ -11,19 +15,36 @@ var departureList = [];
 
 
 $("#city-search").on("click", function () {
+<<<<<<< HEAD
   var multilist = false;
   var multicountries = false;
+=======
+>>>>>>> master
   // ? CREATE/CHECK ELEMENTS FIRST
   // If the elements already exist, remove them
   // ? QUERY APIs
   // ? POPULATE ELEMENTS VIA IDs & CLASSES
   // ? MOVE PAGE VIA JQUERY
+<<<<<<< HEAD
   var city = $("#outboundCity").val();
   var arrivalCity = $("#inboundCity").val();
   var testing = airports.filter(function(something){
     //filters out info for arrival city
     return something.city.includes(arrivalCity);
   })
+=======
+  console.log("click");
+  fnStepTwo(2);
+  var city = $("#outboundCity").val();
+
+  $("html, body").animate(
+    {
+      scrollTop: $(`#step-2`).offset().top,
+    },
+    2000
+  );
+
+>>>>>>> master
   var cityAirportCode = airports.filter(function (forecast) {
     return forecast.city.includes(city);
     // define a variable for airport code
@@ -52,6 +73,7 @@ $("#city-search").on("click", function () {
     console.log(response);
   });
 
+<<<<<<< HEAD
   
   
 
@@ -107,6 +129,27 @@ fnStepTwo(2)
 fnStepThree(3);
 fnStepFour(4);
 
+=======
+  console.log(cityAirportCode);
+});
+
+var settings = {
+  async: true,
+  crossDomain: true,
+  url:
+    "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/DEN/Aba/2020-08-13" /* `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${outbound-city}/${inbound-city}/2020-09-01`,*/,
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    "x-rapidapi-key": "7c47b2fe8fmsh63683bae1988c2fp1fbb32jsn3a82c96b46b9",
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+>>>>>>> master
 //ajax call for Skyscanner
 
 
@@ -116,6 +159,16 @@ function fnSave() {
   // add primary destination to local obj array
 }
 
+function fnReset() {}
+
+function fnTranslate(data) {
+  // get data
+  // origin lang -> new lang
+  // edit data for formatting
+  // ajax request
+  // parse response
+  // send data to data hooks
+}
 
 // step functions
 function fnStepOne() {
@@ -124,6 +177,7 @@ function fnStepOne() {
 
 // 2
 function fnStepTwo(st) {
+<<<<<<< HEAD
   fnCommonRow(st);
   fnCreateParallax(st);
 
@@ -173,10 +227,93 @@ function fnStepTwo(st) {
   btn2.text("Compare");
   el4.append(btn2)
   row2.append(el4)
+=======
+  if ($(`#step-${st}`).length) {
+    // already exists
+    // console.log("it exists");
+  } else {
+    fnCommonRow(st);
+    fnCreateParallax(st);
+
+    let row1 = $(`<div class="row">`);
+    let row2 = $(`<div class="row">`);
+    // attach to #step-${st}
+    $(`#step-${st}`).append(row1);
+    $(`#step-${st}`).append(row2);
+
+    // row 1 // image & flight info
+    // image
+    let el1 = $(`<div class="col m3">`);
+    let imageArea = $(
+      `<img class="primary-destination-img" id="prime-img" src="">`
+    );
+    el1.append(imageArea);
+    // ? DATA HOOKS
+    // ! data query should send to class
+    row1.append(el1);
+
+    // flight info
+    let el2 = $(`<div class="col m9">`);
+    let elHead = $(`<h2 class="primary-destination-head" id="prime-head">`);
+    let elInfo = $(`<p class="primary-destination-info" id="prime-info">`);
+    elInfo.text(
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    );
+    // ? DATA HOOKS
+    // ! data query should send to class
+    el2.append(elHead);
+    el2.append(elInfo);
+    row1.append(el2);
+
+    // row 2 // buttons
+    // more info button
+    let el3 = $(`<div class="col m6" style="text-align:right;">`);
+    let btn1 = $(`<a id="more-info" data-step="3"
+		class="btn-large waves-effect waves-light new-red-lighten-1"
+		>`);
+    btn1.text("See More Info");
+    el3.append(btn1);
+    row2.append(el3);
+    $("#more-info").on("click", function () {
+      // event delegation workaround
+      fnStepThree(3);
+
+      // ! jquery move
+      $("html, body").animate(
+        {
+          scrollTop: $(`#step-3`).offset().top,
+        },
+        2000
+      );
+    });
+
+    // jump to compare button
+    let el4 = $(`<div class="col m6">`);
+    let btn2 = $(`<a id="compare" data-step="4"
+		class="btn-large waves-effect waves-light new-red-lighten-1"
+		>`);
+    btn2.text("Compare");
+    el4.append(btn2);
+    row2.append(el4);
+    $("#compare").on("click", function () {
+      // event delegation workaround
+      fnStepFour(4);
+
+      // ! jquery move
+      $("html, body").animate(
+        {
+          scrollTop: $(`#step-4`).offset().top,
+        },
+        2000
+      );
+    });
+  }
+>>>>>>> master
 }
 
 // 3
 function fnStepThree(st) {
+<<<<<<< HEAD
   fnCommonRow(st);
   fnCreateParallax(st);
 
@@ -216,10 +353,69 @@ function fnStepThree(st) {
   btn.text("Compare");
   ctCol.append(btn)
   row2.append(ltCol, ctCol, rtCol)
+=======
+  if ($(`#step-${st}`).length) {
+    // already exists
+    // console.log("it exists");
+  } else {
+    fnCommonRow(st);
+    fnCreateParallax(st);
+
+    let cardOrder = ["Weather", "Phrases", "Currency"];
+
+    let row1 = $(`<div class="row">`);
+    let row2 = $(`<div class="row">`);
+
+    // attach to #step-${st}
+    console.log($(`#step-${st}`));
+    $(`#step-${st}`).append(row1);
+    $(`#step-${st}`).append(row2);
+
+    // row 1 // cards
+    for (let i = 0; i <= 2; i++) {
+      let col = $(`<div class="col m4">`);
+      let card = $(`<div class="card">`);
+      let cardContent = $(`<div class="card-content">`);
+      let cardTitle = $(`<span class="card-title" id="card-${cardOrder[i]}">`);
+      cardTitle.text("Title");
+      let cardText = $(`<p id="card${cardOrder[i]}">`);
+      cardText.text("I am a card.");
+
+      cardContent.append(cardTitle, cardText);
+      card.append(cardContent);
+      col.append(card);
+      row1.append(col);
+    }
+
+    //row 2 // button
+    let ltCol = $(`<div class="col m5">`);
+    let rtCol = $(`<div class="col m5">`);
+    let ctCol = $(`<div class="col m2">`);
+    let btn = $(`<a id="compare2" data-step="4"
+	class="btn-large waves-effect waves-light new-red-lighten-1"
+	>`);
+    btn.text("Compare");
+    ctCol.append(btn);
+    row2.append(ltCol, ctCol, rtCol);
+    $("#compare2").on("click", function () {
+      // event delegation workaround
+      fnStepFour(4);
+
+      // ! jquery move
+      $("html, body").animate(
+        {
+          scrollTop: $(`#step-4`).offset().top,
+        },
+        2000
+      );
+    });
+  }
+>>>>>>> master
 }
 
-// 4 
+// 4
 function fnStepFour(st) {
+<<<<<<< HEAD
   fnCommonRow(st);
   fnCreateParallax(st);
 
@@ -255,17 +451,66 @@ function fnStepFour(st) {
   // Save
   let el3 = $(`<div class="col m6" style="text-align:right;">`);
   let btn1 = $(`<a id="save-item" data-step="3"
-	class="btn-large waves-effect waves-light new-red-lighten-1"
-	>`);
-  btn1.text("Save");
-  el3.append(btn1);
-  row2.append(el3);
+=======
+  if ($(`#step-${st}`).length) {
+    // already exists
+    // console.log("it exists");
+  } else {
+    fnCommonRow(st);
+    fnCreateParallax(st);
 
-  // Search again button
-  let el4 = $(`<div class="col m6">`);
-  let btn2 = $(`<a id="search-again" data-step="4"
+    let row1 = $(`<div class="row">`);
+    let row2 = $(`<div class="row">`);
+    // attach to #step-${st}
+    $(`#step-${st}`).append(row1);
+    $(`#step-${st}`).append(row2);
+
+    // row 1 // image & flight info
+    // image
+    let el1 = $(`<div class="col m3">`);
+    let imageArea = $(
+      `<img class="primary-destination-img" id="prime-img" src="">`
+    );
+    el1.append(imageArea);
+    // ? DATA HOOKS
+    // ! data query should send to class
+    row1.append(el1);
+
+    // flight info
+    let el2 = $(`<div class="col m9">`);
+    let elHead = $(`<h2 class="primary-destination-head" id="prime-head">`);
+    let elInfo = $(`<p class="primary-destination-info" id="prime-info">`);
+    elInfo.text(
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    );
+    // ? DATA HOOKS
+    // ! data query should send to class
+    el2.append(elHead);
+    el2.append(elInfo);
+    row1.append(el2);
+
+    // row 2 // buttons
+    // Save
+    let el3 = $(`<div class="col m6" style="text-align:right;">`);
+    let btn1 = $(`<a id="save-item" data-step="3"
+>>>>>>> master
 	class="btn-large waves-effect waves-light new-red-lighten-1"
 	>`);
+    btn1.text("Save");
+    el3.append(btn1);
+    row2.append(el3);
+    $("#save-item").on("click", function () {
+      // event delegation workaround
+      fnStepFour(4);
+      fnSave();
+    });
+
+    // Search again button
+    let el4 = $(`<div class="col m6">`);
+    let btn2 = $(`<a id="search-again" data-step="4"
+	class="btn-large waves-effect waves-light new-red-lighten-1"
+	>`);
+<<<<<<< HEAD
   btn2.text("Search Again");
   el4.append(btn2)
   row2.append(el4)
@@ -273,6 +518,44 @@ function fnStepFour(st) {
   // add cards within parallax
   let cardRow = $("#jump-4")
   cardRow = cardRow[0].children[0].children[0]
+=======
+    btn2.text("Search Again");
+    el4.append(btn2);
+    row2.append(el4);
+    $("#search-again").on("click", function () {
+      // ! jquery move
+      $("html, body").animate(
+        {
+          scrollTop: $(`#step-1`).offset().top,
+        },
+        2000
+      );
+      fnReset();
+    });
+
+    // add cards within parallax
+    let cardRow = $("#savedGallery");
+
+    // cards for previous entries
+    for (let i = 1; i <= saved.length; i++) {
+      let col = $(`<div class="col m4">`);
+      let card = $(`<div class="card">`);
+      let cardContent = $(`<div class="card-content">`);
+      let cardTitle = $(`<span class="card-title" id="savedTitle-${i}">`); // ? DATA HOOK
+      cardTitle.text("Title");
+      let cardText = $(`<p id="savedInfo-${i}">`); // ? DATA HOOK
+      cardText.text("I am a card.");
+
+      cardContent.append(cardTitle, cardText);
+      card.append(cardContent);
+      col.append(card);
+      cardRow.append(col);
+      btn2.text("Search Again");
+      el4.append(btn2);
+      row2.append(el4);
+    }
+  }
+>>>>>>> master
 }
 
 // creates the first container and row common to each section, if they don't exist yet
@@ -304,13 +587,16 @@ function fnCreateParallax(s) {
     let el2 = $(`<div class="section no-pad-bot" id='jump-${s}'>`);
     let el3 = $(`<div class="container">`);
     let el4 = $(`<div class="row center">`);
+    if (s == 4) el4.attr("id", "savedGallery");
     el1.append(el2);
     el2.append(el3);
     el3.append(el4);
     let el5 = $(`<div class='parallax'>`);
-    let el6 = $(`<img src='Assets/jumbo-${s}.png'>`);
+    let el6 = $(`<img id='parallax-${s}' src='Assets/jumbo-${s}.png'>`);
     el1.append(el5);
     el5.append(el6);
+    // ! Images not appearing when created dynamically
+    // $(`parallax-${s}`).trigger('resize').trigger('scroll')
 
     el1.append(el2);
     $("#main-content").append(el1);
